2880e6f466b654b35f3bb30eca61e1281c35afb3
boroksid@yandex.ru <boroksid@yandex.ru>
2023-03-14 22:22:03 +0300

    Добавьте файлы проекта.

---
+++ Module_16.Tests/Module_16.Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module_16;
+
+namespace Modul_16_TaskTests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(50, 10) == 60);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(2, 20) == 40);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+
+    }
+}
